570a0f2b77987bbc478a05101833baa2b609bca2
sijiaoh <sijiaoh@outlook.com>
2023-10-26 08:08:25 +0900

    Fix have_editor_type_enum spec matcher.

--- spec/support/matchers/have_editor_type_enum.rb
+++ spec/support/matchers/have_editor_type_enum.rb
@@ -1,5 +1,9 @@
 RSpec::Matchers.define :have_editor_type_enum do
   match do |actual|
-    expect(actual).to enumerize(:editor_type).in(textarea: 0, wysiwyg: 1, markdown: 2).with_default(:textarea)
+    expect(actual).to enumerize(:editor_type).in(
+      textarea: 0,
+      wysiwyg: 1,
+      markdown: 2
+    ).with_default(Rails.configuration.default_editor_type)
   end
 end
