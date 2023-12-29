49e067565b1e564d0846c7ce15a4561ccaf14d64
sijiaoh <sijiaoh@outlook.com>
2023-12-29 17:57:17 +0900

    Remove tinymce locale file.

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin_all_from "app/javascript/utils", under: "utils"
-pin_all_from "vendor/javascript/tinymce_languages", under: "tinymce_languages"
 
 pin "google_analytics", to: "google_analytics.js"
